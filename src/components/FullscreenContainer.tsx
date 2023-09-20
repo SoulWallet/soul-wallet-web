5a2174a8f293c3de99efb7cbe1bb61aeee821db1
Terence Ge <terencegehui@yahoo.com>
2023-09-20 15:41:20 +0800

    chore: move old pages to archive

--- src/components/FullscreenContainer.tsx
+++ src/components/FullscreenContainer.tsx
@@ -15,20 +15,6 @@ export default function FullscreenContainer({ children }: { children: ReactNode 
       height="100vh"
       width="100%"
     >
-      <Box
-        id="animation-portal"
-        position="fixed"
-        top="0"
-        bottom="0"
-        left="0"
-        right="0"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        background="transparent"
-        zIndex="0"
-        pointerEvents="none"
-      />
       <Logo />
       <Box
         background="rgba(255, 255, 255, 0.02)"
