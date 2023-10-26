0bc9613e1a6a2968f211efef95de98a0723205ab
Terence Ge <terencegehui@yahoo.com>
2023-10-26 23:24:45 +0800

    chore: handle upload

--- src/pages/recover/SetPasskeys/index.tsx
+++ src/pages/recover/SetPasskeys/index.tsx
@@ -186,7 +186,7 @@ export default function SetPasskeys({ changeStep }: any) {
         <Button onClick={createWallet} _styles={{ width: '320px', marginBottom: '12px', background: '#9648FA' }} _hover={{ background: '#9648FA' }} disabled={isCreating} loading={isCreating}>
           Add another passkey
         </Button>
-        <Button onClick={handleNext} _styles={{ width: '320px' }} disabled={isCreating}>
+        <Button onClick={handleNext} _styles={{ width: '320px' }} disabled={isCreating || !credentials.length}>
           Continue
         </Button>
       </Box>
