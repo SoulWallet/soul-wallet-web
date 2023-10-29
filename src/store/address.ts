642cd24bf9ed42d5a7620b00ef10579605763036
rocker <exrobbie@gmail.com>
2023-10-29 23:10:42 +0800

    chore: clear user storage

--- src/store/address.ts
+++ src/store/address.ts
@@ -102,6 +102,6 @@ const createAddressSlice = immer<IAddressStore>((set, get) => ({
 export const useAddressStore = create<IAddressStore>()(
   persist((...set) => ({ ...createAddressSlice(...set) }), {
     name: 'address-storage',
-    version: 7,
+    version: 8,
   }),
 );
