1655d8f61d1c789218507368e880a686cba534d6
rocker <exrobbie@gmail.com>
2023-11-05 17:14:13 +0800

    chore: optimized err hint

--- src/components/Uploader/index.tsx
+++ src/components/Uploader/index.tsx
@@ -11,50 +11,50 @@ export default function Uploader({ onUploaded, children, ...restProps }: any) {
     console.log('file is', file);
     const fileExtension = file.type.split('/')[1];
     console.log(fileExtension);
-    const res = await api.operation.fileUploadUrl({
-      fileExtension,
-    });
 
-    if(res.code !== 200){
+    try {
+      const res = await api.operation.fileUploadUrl({
+        fileExtension,
+      });
+
+      const { fileKey, url, accessUrl } = res.data;
+      console.log('file key and url', fileKey, url);
+
+      const result = await fetch(url, {
+        method: 'PUT',
+        body: file,
+        headers: {
+          'Content-Type': file.type,
+        },
+      });
+
+      console.log('upload result', result);
+
+      if (result.status === 200) {
+        // do upload logic
+        onUploaded({
+          fileKey,
+          fileType: file.type,
+          accessUrl,
+        });
+      } else {
+        toast({
+          title: 'error',
+          title: 'Failed to upload to server',
+        });
+      }
+    } catch (err) {
       toast({
         status: 'error',
-        title: res.msg,
-      })
-      return
-    }
-
-    const { fileKey, url, accessUrl } = res.data;
-    console.log('file key and url', fileKey, url);
-
-    const result = await fetch(url, {
-      method: 'PUT',
-      body: file,
-      headers: {
-        'Content-Type': file.type,
-      },
-    });
-
-    console.log('upload result', result);
-
-    if (result.status === 200) {
-      // do upload logic
-      onUploaded({
-        fileKey,
-        fileType: file.type,
-        accessUrl,
+        title: err.response.data.msg,
       });
-    }else{
-      toast({
-        title: 'error',
-        title: "Failed to upload to server"
-      })
     }
   };
 
   const handleFileInput = async (e: any) => {
     // e.target.files.map((item) => uploadToServer(item));
     const file = e.target.files[0];
-    await uploadToServer(file)
+    await uploadToServer(file);
   };
 
   const handleClick = () => {
