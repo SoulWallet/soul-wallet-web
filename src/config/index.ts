f5fec1e554cf5c1a23bbdb16322ee62616d28478
rocker <exrobbie@gmail.com>
2023-11-14 18:47:55 +0800

    chore: update version

--- src/config/index.ts
+++ src/config/index.ts
@@ -50,7 +50,7 @@ export const chainMapping = {
   },
 };
 // This is an important store switch which will clear all users' data
-export const storeVersion = '2';
+export const storeVersion = '3';
 
 export default {
   homepage: "https://www.soulwallet.io",
