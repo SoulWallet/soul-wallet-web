044d844c9dec6e7d89301ff160672e39b35d3990
rocker <exrobbie@gmail.com>
2023-11-22 18:08:36 +0800

    chore: clear store

--- src/config/index.ts
+++ src/config/index.ts
@@ -50,7 +50,7 @@ export const chainMapping = {
   },
 };
 // This is an important store switch which will clear all users' data
-export const storeVersion = '8';
+export const storeVersion = '10';
 
 export const defaultGuardianSafePeriod = 1;
 
