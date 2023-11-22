003a47826185b44a989409837ea1f917d1a7beed
rocker <exrobbie@gmail.com>
2023-11-22 17:42:55 +0800

    feat: optimized balance card

--- src/pages/wallet/comp/Balance/index.tsx
+++ src/pages/wallet/comp/Balance/index.tsx
@@ -1,12 +1,29 @@
-import { Box, Flex, Text, useToast, Image } from '@chakra-ui/react';
+import { Box, Flex, Text, useToast, Image, Link } from '@chakra-ui/react';
 import HomeCard from '../HomeCard';
 import Button from '@/components/Button';
 import { ethers } from 'ethers';
 import useWalletContext from '@/context/hooks/useWalletContext';
+import IconScan from '@/assets/icons/scan.svg';
+import IconShare from '@/assets/icons/share.svg';
 import { useCredentialStore } from '@/store/credential';
+import useConfig from '@/hooks/useConfig';
+import { useAddressStore } from '@/store/address';
+
+const Btn = ({ children, ...restProps }: any) => {
+  return (
+    <Box p="10px" bg="#f2f2f2" cursor={'pointer'} rounded="full" {...restProps}>
+      {children}
+    </Box>
+  );
+};
+
 export default function Balance() {
   const { showTransferAssets } = useWalletContext();
   const { walletName } = useCredentialStore();
+  const { selectedAddress } = useAddressStore();
+  const { chainConfig } = useConfig();
+  const { scanUrl } = chainConfig;
+
   return (
     <HomeCard
       title={'Balance'}
@@ -16,43 +33,55 @@ export default function Balance() {
       px="8"
       overflow="none"
       contentHeight="160px"
+      pos="relative"
     >
-      <Box p="4">
-        {walletName && (
-          <Text color="#000" fontSize={'16px'} fontWeight={'600'} mb="4" fontFamily={'Martian'}>
-            {walletName}
-          </Text>
-        )}
-        <Text mt={{ base: 4, lg: 'unset' }} fontSize={'48px'} fontWeight={'800'} lineHeight={'1'}>
-          $0
+      {walletName && (
+        <Text color="#000" fontSize={'16px'} fontWeight={'600'} mb="4" fontFamily={'Martian'}>
+          {walletName}
         </Text>
-        {/* <Flex fontSize={'16px'} gap="1">
+      )}
+      <Text mt={{ base: 4, lg: 'unset' }} fontSize={'48px'} fontWeight={'800'} lineHeight={'1'}>
+        $0
+      </Text>
+      {/* <Flex fontSize={'16px'} gap="1">
         <Text color="#10c003" fontWeight={'700'}>
           + $0 (0%)
         </Text>
         <Text fontWeight={'600'}>Past day</Text>
       </Flex> */}
-        <Flex gap="2" mt="6" mb="2" align={'center'} display={{ base: 'flex', lg: 'none' }}>
-          <Button
-            flex="1"
-            py="3"
-            onClick={() => {
-              showTransferAssets(ethers.ZeroAddress, 'receive');
-            }}
-          >
-            Receive
-          </Button>
-          <Button
-            flex="1"
-            py="3"
-            onClick={() => {
-              showTransferAssets(ethers.ZeroAddress, 'send');
-            }}
-          >
-            Send
-          </Button>
-        </Flex>
-      </Box>
+      <Flex gap="2" align={'center'} pos="absolute" right="4" bottom="3" display={{ base: 'none', lg: 'flex' }}>
+        <Btn
+          onClick={() => {
+            showTransferAssets(ethers.ZeroAddress, 'receive');
+          }}
+        >
+          <Image src={IconScan} />
+        </Btn>
+        <Btn as={Link} href={`${scanUrl}/address/${selectedAddress}`} target="_blank">
+          <Image src={IconShare} />
+        </Btn>
+      </Flex>
+
+      <Flex gap="2" mt="6" mb="2" align={'center'} display={{ base: 'flex', lg: 'none' }}>
+        <Button
+          flex="1"
+          py="3"
+          onClick={() => {
+            showTransferAssets(ethers.ZeroAddress, 'receive');
+          }}
+        >
+          Receive
+        </Button>
+        <Button
+          flex="1"
+          py="3"
+          onClick={() => {
+            showTransferAssets(ethers.ZeroAddress, 'send');
+          }}
+        >
+          Send
+        </Button>
+      </Flex>
     </HomeCard>
   );
 }
