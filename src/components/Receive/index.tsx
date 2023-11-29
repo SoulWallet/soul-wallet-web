0d4ce7029691d5e3462c1a2837491470968e52cf
rocker <exrobbie@gmail.com>
2023-11-29 15:56:46 +0800

    chore: allow user edit name

--- src/components/Receive/index.tsx
+++ src/components/Receive/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Text, Flex, useToast, Tooltip, Link } from '@chakra-ui/react';
+import { Box, Text, Flex, useToast, Tooltip, Link, Input } from '@chakra-ui/react';
 import { InfoWrap, InfoItem } from '@/components/SignTransactionModal';
+import { useState } from 'react';
 import { Image } from '@chakra-ui/react';
 import { copyText } from '@/lib/tools';
 import ReceiveCode from '@/components/ReceiveCode';
@@ -7,14 +8,17 @@ import Button from '@/components/Button';
 import { useAddressStore } from '@/store/address';
 import { useChainStore } from '@/store/chain';
 import useConfig from '@/hooks/useConfig';
+import IconPen from '@/assets/icons/pen.svg';
 import IconShare from '@/assets/icons/share.svg';
 
 export default function Receive() {
   const toast = useToast();
-  const { selectedAddress } = useAddressStore();
+  const { selectedAddress, updateAddressItem } = useAddressStore();
   const { chainList } = useChainStore();
   const { selectedAddressItem, chainConfig } = useConfig();
   const { scanUrl, scanName } = chainConfig;
+  const [editing, setEditing] = useState(false);
+  const [editingName, setEditingName] = useState(selectedAddressItem.title);
 
   const doCopy = () => {
     copyText(selectedAddress);
@@ -24,11 +28,31 @@ export default function Receive() {
     });
   };
 
+  const doEdit = () => {
+    updateAddressItem(selectedAddress, { title: editingName });
+    setEditing(false);
+  };
+
   return (
     <Box w="320px" mx="auto">
-      <Text mb="3" fontWeight={'800'} fontSize={'20px'} textAlign={'center'}>
-        {selectedAddressItem.title}
-      </Text>
+      <Flex gap="2" mb="3" align={'center'} justify={'center'}>
+        {editing ? (
+          <Input value={editingName} onChange={(e) => setEditingName(e.target.value)} />
+        ) : (
+          <Text fontWeight={'800'} fontSize={'20px'} textAlign={'center'}>
+            {selectedAddressItem.title}
+          </Text>
+        )}
+
+        {editing ? (
+          <Button py="2" onClick={doEdit}>
+            Done
+          </Button>
+        ) : (
+          <Image cursor={'pointer'} src={IconPen} onClick={() => setEditing(true)} />
+        )}
+      </Flex>
+
       <Box bg="#ededed" rounded="20px" p="4" mb="14px" py="6" textAlign={'center'} mx="auto">
         <ReceiveCode address={selectedAddress} imgWidth="160px" showFullAddress={true} />
       </Box>
@@ -59,7 +83,7 @@ export default function Receive() {
         href={`${scanUrl}/address/${selectedAddress}`}
         target="_blank"
       >
-        <Text lineHeight={"1"} fontSize={'18px'} fontWeight={'600'} textDecoration={'none'}>
+        <Text lineHeight={'1'} fontSize={'18px'} fontWeight={'600'} textDecoration={'none'}>
           {scanName}
         </Text>
         <Image src={IconShare} />
