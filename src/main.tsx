b1545c5cacdc9915de8c65ee7c1e4eaad3920ece
rocker <exrobbie@gmail.com>
2023-12-06 19:05:12 +0800

    chore: enable mapset

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { router } from './router.tsx';
 import { RouterProvider } from 'react-router-dom';
+import { enableMapSet } from 'immer'
+
+enableMapSet();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // StrictMode will cause double render
