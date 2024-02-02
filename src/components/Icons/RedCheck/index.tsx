5b18d9a57dec9e9936398385515d137ce3fef68c
Terence Ge <terencegehui@yahoo.com>
2024-02-02 20:50:35 +0800

    chore: change add passkey

---
+++ src/components/Icons/RedCheck/index.tsx
@@ -0,0 +1,9 @@
+const RedCheck = () => {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M5 10.75L8.5 14.25L15 5.75" stroke="#FF2E79" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  );
+};
+
+export default RedCheck;
