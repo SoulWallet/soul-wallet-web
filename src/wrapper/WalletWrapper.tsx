eba6048960325455eff8bd0ade0e79aecbebac4a
rocker <exrobbie@gmail.com>
2024-03-19 21:31:37 +0800

    chore: optimized logic

--- src/wrapper/WalletWrapper.tsx
+++ src/wrapper/WalletWrapper.tsx
@@ -3,13 +3,16 @@ import { Outlet } from 'react-router-dom';
 import FindRoute from '@/components/FindRoute';
 import Pooling from '../components/Pooling';
 import CommonWrapper from './CommonWrapper';
+import { Box } from '@chakra-ui/react';
 
 export default function Wrapper() {
   return (
     <CommonWrapper>
       <FindRoute>
         <WalletContextProvider>
-          <Outlet />
+          <Box maxW={"430px"} mx="auto">
+            <Outlet />
+          </Box>
           <Pooling />
         </WalletContextProvider>
       </FindRoute>
